4fbe1bdceb863095efa8958df7a25249137dec65
Madhav Negi <maddynegi03@gmail.com>
2024-09-11 22:43:46 +0530

    some filtering features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,79 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import { data,categories } from './Data' 
+import ProductCard from './components/ProductCard';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [products, setProducts] = useState(null);
+  const [selectedCategories,setSelectedCategories]=useState([]);
+  const [selectedRating,setSelectedRating]=useState(0);
+  
+  const handleChange=(e)=>{
+    const {value,checked}=e.target;
+    if(checked){
+      setSelectedCategories([...selectedCategories,value]);
+    }
+    else{
+      setSelectedCategories(selectedCategories.filter((option)=>option!==value));
+    }
+  }
+  const handleRatingChange=(e)=>{
+    const {value,checked}=e.target;
+    if(checked){
+      setSelectedRating(value);
+    }
+    else{
+      setSelectedRating(0);
+    }
+  }
+  console.log('ratings',selectedRating)
+  useEffect(()=>{
+    let newData=data;    
+    if(selectedCategories.length>0){
+      newData=newData.filter((option)=>selectedCategories.includes(option.category))
+    }
+    if(selectedRating!=0){
+      newData=newData.filter((option)=>option.rating.rate>selectedRating)
+    }
+    setProducts(newData);
+  },[selectedCategories,selectedRating])
+  
   return (
-    <>
-      <div></div>
-    </>
+    <div className='flex flex-wrap mt-10 w-full'>
+      <div className='h-lvh w-[200px] basis-1/4 text-left px-4'>
+        <h1 className='text-xl font-bold'>Categories</h1>
+        <div className='flex flex-col items-start '>
+        {
+          categories.map((val,key)=>{
+            return <label>
+                  <input onChange={handleChange} type='checkbox' key={key} value={val}/> {val}
+                </label>
+          })
+        }
+        </div>
+        <h1 className='text-xl font-bold mt-2'>Ratings</h1>
+        <div className='flex flex-col items-start '>
+          <label>
+            <input onChange={handleRatingChange} checked={selectedRating==4} type='checkbox' value={4}/> 4 and above 
+          </label>
+          <label>
+            <input onChange={handleRatingChange} checked={selectedRating==3} type='checkbox' value={3}/> 3 and above 
+          </label>
+          <label>
+            <input onChange={handleRatingChange} checked={selectedRating==2} type='checkbox' value={2}/> 2 and above 
+          </label>
+          <label>
+            <input onChange={handleRatingChange} checked={selectedRating==1} type='checkbox' value={1}/> 1 and above 
+          </label>
+        </div>
+      </div>
+      <div className='flex basis-3/4 w-full justify-around flex-wrap '>
+        { products && 
+          products.map((value,key)=>{
+            return <ProductCard info={value} key={key}/>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,34 @@
+const ProductCard = ({info}) => {
+  const data={
+  "id": 1,
+  "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  "price": 109.95,
+  "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  "category": "men's clothing",
+  "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "rating": {
+    "rate": 3.9,
+    "count": 120
+  }
+}
+  return (
+    <div className="max-w-56 mr-4 basis-1/4 mb-4 bg-white text-black px-4 py-3 rounded-lg border">
+      <div className="h-[200px] flex justify-center">
+        <img src={info.image} className="h-full"/>    
+      </div>
+      <div className="mt-5">
+        <p className="text-ellipsis overflow-hidden whitespace-nowrap text-l">{info.title}</p>
+        <div className="text-left mt-2">
+          <p className="overflow-hidden text-xs text-gray-400">Rating {info.rating.rate} ({info.rating.count})</p>
+          <p className="overflow-hidden text-sm font-bold">${info.price}</p>
+        </div>
+        <div className="flex justify-between mt-2">
+          <button className="bg-yellow-500 px-2 py-2 rounded-lg text-white">Wishlist</button>
+          <button className="bg-blue-400 px-2 py-2 rounded-lg text-white">Buy now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
